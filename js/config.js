35b00964e520f8719bbb255b911b15dd32b99808
Grecagni <151087457+Grecagni@users.noreply.github.com>
2025-08-25 21:21:19 +0200

    Use correct Firebase storage bucket

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyB76HrqxMp0EuPsedVMBJaz6MW5qHdFr10",
   authDomain: "gestione-spese-personale.firebaseapp.com",
   projectId: "gestione-spese-personale",
-  storageBucket: "gestione-spese-personale.firebasestorage.app",
+  storageBucket: "gestione-spese-personale.appspot.com",
   messagingSenderId: "659511690509",
   appId: "1:659511690509:web:c1eb2a8c1b0b10ce469c44"
 };
